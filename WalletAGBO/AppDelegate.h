6fae53f83d4f80651249efac09f278ed41062aac
RAMON ALBERTI DANES <firebird@MacBook-Pro-de-RAMON.local>
2015-06-03 21:22:47 +0200

    Initial Commit

---
+++ WalletAGBO/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WalletAGBO
+//
+//  Created by RAMON ALBERTI DANES on 3/6/15.
+//  Copyright (c) 2015 Krainet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
